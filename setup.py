daab50cb049a1716abf085b0e8fd4579c8dae86b
Synthbot Anon <synthbot.anon@gmail.com>
2022-06-30 13:56:15 -0700

    make library compatible with setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kdtree',
+    version='0.0.1',
+    description="A clone of Vectorized's kdtree that can be installed with pip.",
+    url='https://github.com/synthbot-anon/Vectorized-Python-KD-Tree',
+    author='Synthbot Anon',
+    author_email='synthbot.anon@gmail.com',
+    packages=['kd_tree'],
+    package_dir={'': 'src'},
+)
